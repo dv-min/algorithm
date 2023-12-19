1e57194b0241975b10e21af12f64a7c8fd364996
DevMin <dev-min>
2023-12-19 16:58:57 +0900

    20231219_1658

--- baekjoon/2476.py
+++ baekjoon/2476.py
@@ -1,26 +1,18 @@
-n = int(input())
+case = int(input())
+answer = 0
 
-rs = []
-for i in range(n):
-    a, b, c = list(map(int, input().split()))
-    if a == b and a == c and b == c:
-        r = 10000 + a * 1000
-    elif a != b and a != c and b != c:
-        if a > b and a > c:
-            r = a
-        elif b > a and b > c:
-            r = b
-        else:
-            r = c
-        r = r * 100
+for _ in range(case):
+    a, b, c = map(int, input().split())
+
+    if a == b == c:
+        answer = max(answer, 10000 + a * 1000)
+    elif a == b:
+        answer = max(answer, 1000 + a * 100)
+    elif a == c:
+        answer = max(answer, 1000 + a * 100)
+    elif b == c:
+        answer = max(answer, 1000 + b * 100)
     else:
-        if a == b and a == c:
-            r = a
-        elif b == a and b == c:
-            r = b
-        else:
-            r = c
-        r = 1000 + r * 100
-    rs.append(r)
+        answer = max(answer, 100 * max(a, b, c))
 
-print(max(rs))
+print(answer)
